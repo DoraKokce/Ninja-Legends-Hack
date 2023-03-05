428d8dc450bf5cf9d193e87460fe17e0e91acee5
DoraKokce <92853606+DoraKokce@users.noreply.github.com>
2023-03-05 20:19:12 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,50 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("DoraxHub | Ninja Legends", "Sentinel")
+local Farm = Window:NewYab("Auto Farm")
+local FarmSection = Farm:NewSection("Farm")
+FarmSection:NewToggle("Auto Swing","Make your player autoswing", function(v)
+  getgenv().autoswing = v
+  while true do
+      if not getgenv().autoswing then return end
+      for _.v in pairs(game.Players.LocalPlayer.Bacpack:GetChildren()) do
+        if v:FindFirstChild("ninjitsuGain") then 
+          game.Player.LocalPlayer.Character.Humanoid:EquipTool(v)
+          break
+        end
+      end
+      local A_1 = "swingKatana"
+      local Event = game:GetServide("Players").LocalPlayer.ninjaEvent
+      wait(0.1)
+  end
+end)
+FarmSection:NewToggle("Auto Sell", "Makes your player autosell", function(v)
+        getgenv().autosell = v
+        while true do
+            if getgenv().autoswing == false then return end
+            game:GetService("Workspace").sellAreaCircles["sellAreaCircle16"].circleInner.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            wait(0.1)
+            game:GetService("Workspace").sellAreaCircles["sellAreaCircle16"].circleInner.CFrame = CFrame.new(0,0,0)
+            wait(0.1)
+        end
+end)
+
+FarmSection:NewButton("Unlock all islands", "Unlocks all islands", function()
+        local oldcframe = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        for _,v in pairs(game:GetService("Workspace").islandUnlockParts:GetChildren()) do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+            wait(0.1)
+        end
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = oldcframe
+    end)
+FarmSection:NewToggle("Auto buy all belts", "Auto buys all belts", function(v)
+        getgenv().buybelts = v
+        while true do
+            if not getgenv().buybelts then return end
+            local A_1 = "buyAllBelts"
+            local A_2 = "Inner Peace Island"
+            local Event = game:GetService("Players").LocalPlayer.ninjaEvent
+            Event:FireServer(A_1, A_2)
+            wait(0.5)
+        end
+    end)
